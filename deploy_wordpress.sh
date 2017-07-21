9009a236d7e8ccdf0e57b2b09f582f7e78e87327
WellPolishedMirror <WellPolishedMirror@gmail.com>
2017-07-21 00:22:40 +0000

    Script creates database, users, and wordpress container.

--- deploy_wordpress.sh
+++ deploy_wordpress.sh
@@ -21,11 +21,26 @@ then
 	then
 		docker start "test_sql"
 	else
+		echo "Setting up database..."
 		DB_PW=$(gen_pw)
 		echo $DB_PW > db_passwd.txt
-		docker run --name "test_sql" -e MYSQL_ROOT_PASSWORD=$DB_PW -d mariadb:latest
+		docker run --name "test_sql" -h testdb -e MYSQL_ROOT_PASSWORD=$DB_PW -d mariadb:latest
+		# Wait for database to come up
+		sleep 30
 	fi
 fi
 
-# TODO Create new database and database user
-# TODO Create wordpress container
+# Create new database and database user
+DB_PW=`cat db_passwd.txt`
+docker exec test_sql sh -c "mysql -uroot -p$DB_PW -e \"CREATE DATABASE pac\"" || exit 1 
+docker exec test_sql sh -c "mysql -uroot -p$DB_PW -e \"GRANT ALL PRIVILEGES ON pac.* to pacuser@'%' IDENTIFIED BY 'password'\"" pac || exit 1
+docker exec test_sql sh -c "mysql -uroot -p$DB_PW -e \"GRANT ALL PRIVILEGES ON pac.* to pacuser@'%' IDENTIFIED BY 'password';GRANT ALL PRIVILEGES ON pac.* to pacuser@'localhost' IDENTIFIED BY 'password';\"" pac || exit 1
+
+# Create wordpress container
+docker run --name "test-wordpress" \
+           -p 5000:80 \
+           --link test_sql:mysql \
+           -e WORDPRESS_DB_USER="pacuser" \
+           -e WORDPRESS_DB_PASSWORD="password" \
+           -e WORDPRESS_DB_NAME="pac" \
+           -d wordpress:latest
